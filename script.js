465af674c0cda10e4eb12aa76574c5558de16ef2
Vishal Sharma <visharma2025@gmail.com>
2026-03-15 22:43:36 +0530

    some minor changes to all file to add functionality

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navLinks = document.getElementById("navLinks");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+link.addEventListener("click", () => {
+navLinks.classList.remove("active");
+});
+});
